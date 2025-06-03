e71c723659e1824375f9e54c68ed87096bfd3437
Zevhys <rakha2728@gmail.com>
2025-06-03 22:13:34 +0800

    update

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const {
 } = process.env;
 
 const wakatime = new WakaTimeClient(wakatimeApiKey);
-
 const octokit = new Octokit({ auth: `token ${githubToken}` });
 
 async function main() {
-  const stats = await wakatime.getMyStats({ range: LAST_30_DAYS });
+  const stats = await wakatime.getMyStats({ range: RANGE.LAST_30_DAYS });
   await updateGist(stats);
 }
 
@@ -30,33 +29,29 @@ async function updateGist(stats) {
   }
 
   const emotes = {
-    Python: "🐍",
     JavaScript: "✨",
-    Other: "🔧",
+    TypeScript: "🔷",
+    Python: "🐍",
     Java: "☕",
-    HTML: "🌐",
-    CSS: "🎨"
+    "C++": "⚡",
+    "C#": "💜",
+    Other: "🔧"
   };
 
   const lines = [];
   for (let i = 0; i < Math.min(stats.data.languages.length, 5); i++) {
     const data = stats.data.languages[i];
     const { name, percent, text: time } = data;
-
     const emote = emotes[name] || "🔸";
-
     const line = [
       emote + " " + trimRightStr(name, 10).padEnd(10),
       time.padEnd(14),
       generateBarChart(percent, 21),
       String(percent.toFixed(1)).padStart(5) + "%"
     ];
-
     lines.push(line.join(" "));
   }
-
   if (lines.length === 0) return;
-
   try {
     // Get original filename to update that same file
     const filename = Object.keys(gist.data.files)[0];
@@ -76,14 +71,12 @@ async function updateGist(stats) {
 
 function generateBarChart(percent, size) {
   const syms = "░▏▎▍▌▋▊▉█";
-
   const frac = Math.floor((size * 8 * percent) / 100);
   const barsFull = Math.floor(frac / 8);
   if (barsFull >= size) {
     return syms.substring(8, 9).repeat(size);
   }
   const semi = frac % 8;
-
   return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
     .join("")
     .padEnd(size, syms.substring(0, 1));
